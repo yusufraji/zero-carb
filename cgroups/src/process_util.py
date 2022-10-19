e61d23a2e60b2d60d1ddb0cd2bf8c7f962a3b6e6
Yusuf Raji <raji.yusuf234@gmail.com>
2022-10-19 20:44:07 +0800

    Get parent PID of all running app

---
+++ cgroups/src/process_util.py
@@ -0,0 +1,29 @@
+import getpass
+
+import psutil
+
+
+def get_genesis():
+    genesis = list()
+
+    for process in psutil.process_iter(["name", "username"]):
+
+        _genesis = dict()
+        # The processes with 0 parents are "systemd" and "kthreadd" owned
+        # by root.
+        # The next processes owned by the user will have at least one
+        # parent, of these processes, the ones with only one parent are
+        # those owned by root ("systemd" and "<gui-desktop>",
+        # "gnome-keyring-daemon" in my case).
+        # So, to find the genesis processes of other programs, i'm filtering
+        # by processes with exactly 2 parents and belonging to the current user.
+
+        if (process.info["username"] == getpass.getuser()) and (
+            len(process.parents()) == 2
+        ):
+            _genesis["pid"] = process.pid
+            _genesis["user"] = process.name()
+            _genesis["username"] = process.username()
+            genesis.append(_genesis)
+
+    return genesis
