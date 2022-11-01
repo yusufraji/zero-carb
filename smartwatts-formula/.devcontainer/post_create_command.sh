2d2bf30a029a0d7c3ac94c9b4df082bd82138545
Yusuf Raji <raji.yusuf234@gmail.com>
2022-11-01 20:16:39 +0800

    Run local smartwatts-formula & powerapi with docker compose

---
+++ smartwatts-formula/.devcontainer/post_create_command.sh
@@ -0,0 +1,3 @@
+pip install -e /workspaces/zero-carb/smartwatts-formula/smartwatts-formula/
+pip install -e /workspaces/zero-carb/smartwatts-formula/openapi-client/
+pip install -e "/workspaces/zero-carb/smartwatts-formula/powerapi[mongodb, influxdb, opentsdb, prometheus, influxdb_client]"
